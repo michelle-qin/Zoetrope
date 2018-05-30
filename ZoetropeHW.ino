72e43ab6cf61b153f442a238d083886901a52024
brandontat5 <brandontat5@yahoo.com>
2018-05-30 14:27:27 -0700

    added support for continuous pot and corrected the range for the motor

--- ZoetropeHW.ino
+++ ZoetropeHW.ino
@@ -17,17 +17,19 @@
 #define MOTOR_SPEED_POT A0
 #define MOTOR_PORT 1
 
-#define MICROSTEPPING 32
+#define MICROSTEPPING 4
 #define TRANSMISSION 5
 #define BASE_STEPS_PER_REV 200
-#define MIN_SPEED 0.075
+#define MIN_SPEED 0.2
 
-int beamState = LOW;
-int lastBeamState = LOW;
+bool beamState = LOW;
+bool lastBeamState = LOW;
 bool beamCount = false;
+
 int backlightState = LOW;
 int lastBacklightState = LOW;
 bool backlightCount = false;
+
 int motorState = LOW;
 int lastMotorState = LOW;
 bool motorCount = false;
@@ -35,10 +37,23 @@ bool motorCount = false;
 int potVal = 0;
 int lastPotVal = 0;
 
+int prevPotVal = analogRead(MOTOR_SPEED_POT);
+
+int outputVal = 0;
+int maxOutputVal = 100;
+
+int overflowThreshold = 1023 / 2;
+int filterThreshold = 3;
+
+
+unsigned long startTime;
+
 FlexyStepper stepper;
 
 void setup() {
   ////// Pinmode all ports ///////////
+
+  //Serial.begin(9600);
 
   pinMode(MOTOR_BUTTON_LIGHT, OUTPUT);
   pinMode(MOTOR_BUTTON, INPUT_PULLUP);
@@ -58,10 +73,10 @@ void setup() {
 
   stepper.connectToPort(MOTOR_PORT);
   stepper.setStepsPerRevolution(BASE_STEPS_PER_REV * TRANSMISSION * MICROSTEPPING);
-  stepper.setSpeedInRevolutionsPerSecond(1.0);
+  stepper.setSpeedInRevolutionsPerSecond(3);
   stepper.setAccelerationInRevolutionsPerSecondPerSecond(0.2);
   stepper.setCurrentPositionInRevolutions(0.0);
-  stepper.setTargetPositionRelativeInRevolutions(1000.0);
+  stepper.setTargetPositionRelativeInRevolutions(100000.0);
   stepper.disableStepper();
 
   ///////// Initialize Lights and Button Lights //////////
@@ -72,15 +87,25 @@ void setup() {
   digitalWrite(BACKLIGHT_BUTTON_LIGHT, backlightState);
   digitalWrite(BACKLIGHT_LEDS, backlightState);
 
+  startTime = millis();
 }
 
 void loop() {
 
-  //////////// Non-Latching Buttons require Button States /////////
+  ////////// Non-Latching Buttons require Button States /////////
+  stepper.processMovement();
+
+  if (millis() - startTime > 100) {
+    updateBeamState();
+    updateBacklightState();
+    updateMotorState();
+    startTime = millis();
+  }
+
+}
+
+void updateBeamState() {
   beamState = !digitalRead(BEAM_BUTTON);
-  backlightState = !digitalRead(BACKLIGHT_BUTTON);
-  motorState = !digitalRead(MOTOR_BUTTON);
-  potVal = analogRead(MOTOR_SPEED_POT);
 
   if (beamState != lastBeamState && beamState == HIGH) {
     beamCount = !beamCount;
@@ -95,6 +120,13 @@ void loop() {
     }
   }
 
+  lastBeamState = beamState;
+  //stepper.processMovement();
+}
+
+void updateBacklightState() {
+  backlightState = !digitalRead(BACKLIGHT_BUTTON);
+
   if (backlightState != lastBacklightState && backlightState == HIGH) {
     backlightCount = !backlightCount;
 
@@ -107,7 +139,14 @@ void loop() {
       digitalWrite(BACKLIGHT_LEDS, HIGH);
     }
   }
+  lastBacklightState = backlightState;
+  //stepper.processMovement();
+}
 
+void updateMotorState() {
+  motorState = !digitalRead(MOTOR_BUTTON);
+  potVal = analogRead(MOTOR_SPEED_POT);
+  filterPotVal();
   if (digitalRead(LID_SENSOR)) { // if lid is open disable shit
     motorCount = false;
     digitalWrite(MOTOR_BUTTON_LIGHT, LOW);
@@ -130,14 +169,33 @@ void loop() {
     delay(50); // debouncing delay
   }
 
-  if (motorCount && potVal != lastPotVal) stepper.setSpeedInRevolutionsPerSecond(MIN_SPEED + (float)potVal / 1500.0);
-
-  stepper.processMovement();
-
-  ///////////// Save Last States For Next Loop Iteration /////////////
+  if (motorCount && potVal != lastPotVal) {
+    stepper.setSpeedInRevolutionsPerSecond(MIN_SPEED + (2 * (float)outputVal / 100.0));
+  }
   lastMotorState = motorState;
-  lastBeamState = beamState;
-  lastBacklightState = backlightState;
   lastPotVal = potVal;
-
+  //stepper.processMovement();
 }
+
+
+
+void updateStepperMovement() {
+  stepper.processMovement();
+}
+
+void filterPotVal() {
+  int newPotVal = analogRead(A0);
+
+  if (abs(newPotVal - prevPotVal) > overflowThreshold) {
+    if (prevPotVal > overflowThreshold) outputVal = min(outputVal + 1, maxOutputVal);
+    else outputVal = max(outputVal - 1, 0);
+  }
+
+  else if (abs(newPotVal - prevPotVal) > filterThreshold) {
+    if (newPotVal > prevPotVal) outputVal = min(outputVal + 1, maxOutputVal);
+    else outputVal = max(outputVal - 1, 0);
+  }
+
+  prevPotVal = newPotVal;
+}
+
